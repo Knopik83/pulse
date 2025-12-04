54faf8a04a058ba65caf36ea11ea9ca26f42d8ed
knopik <example@email.com>
2025-12-04 18:02:12 +0300

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,39 @@
+const gulp        = require('gulp');
+const browserSync = require('browser-sync');
+const sass        = require('gulp-sass')(require('sass'));
+const cleanCSS = require('gulp-clean-css');
+const autoprefixer = require('gulp-autoprefixer');
+const rename = require("gulp-rename");
+
+function server() {
+
+    browserSync({
+        server: {
+            baseDir: "src"
+        },
+        browser: ["chrome"],
+        cors: true,
+        headers: {
+            "Content-Security-Policy": "default-src * 'unsafe-inline' 'unsafe-eval' data: blob:"
+        }
+    });
+    gulp.watch("src/sass/**/*.+(scss|sass)", styles);
+    gulp.watch("src/*.html").on('change', browserSync.reload);
+};
+
+function styles() {
+    return gulp.src("src/sass/**/*.+(scss|sass)")
+        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe(rename({suffix: '.min', prefix: ''}))
+        .pipe(autoprefixer())
+        .pipe(cleanCSS({compatibility: 'ie8'}))
+        .pipe(gulp.dest("src/css"))
+        .pipe(browserSync.stream());
+};
+
+
+
+
+exports.server = server;
+exports.styles = styles;
+exports.default = gulp.parallel(server, styles);
